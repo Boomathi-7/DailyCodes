f9427d3f81820a3d3c5a1fd8a936a2c5c65ad139
Boomathi <boomathi2047@gmail.com>
2026-01-23 21:07:07 +0530

    Add variable declarations and print statements

---
+++ C/Variable.c
@@ -0,0 +1,12 @@
+//Declaring a variable and printing
+
+#include <stdio.h>
+int main(){
+  int num = 14;
+  char name[] = "Boomathi";
+  float num2 = 2.7;
+  printf("Integer: %d\n", num);
+  printf("String: %s\n", name);
+  printf("Float: %.2f\n", num2);
+  return 0;
+}
