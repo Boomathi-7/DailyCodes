fa07f067ef8a4005d1545d43bf7794cccf37bf08
Boomathi <boomathi2047@gmail.com>
2026-01-25 22:00:43 +0530

    Add arithmetic operations for two numbers

---
+++ Python/Day_4.py
@@ -0,0 +1,11 @@
+#Arithmatic Operations
+
+num1 = int(input("Enter num1: "))
+num2 = int(input("Enter num2: "))
+print("Addition is", num1+num2)
+print("Difference is", num1-num2)
+print("Multiplication is", num1*num2)
+print("Division is", num1/num2)
+print("Floor Division is", num1//num2)
+print("Remainder on division is", num1%num2)
+print("Exponential is", num1**num2)
