0b9cc3d07499274c31c7d4c583198540db4698d1
Boomathi <boomathi2047@gmail.com>
2026-01-25 22:09:22 +0530

    Implement basic arithmetic operations in C

---
+++ C/Day_4.c
@@ -0,0 +1,16 @@
+//Arithmatic Operators
+
+#include <stdio.h>
+int main(){
+  int num1, num2;
+  printf("Enter num1: ");
+  scanf("%d", &num1);
+  printf("Enter num2: ");
+  scanf("%d", &num2);
+  printf("Addition is %d\n", num1+num2);
+  printf("Difference is %d\n", num1-num2);
+  printf("Multiplication is %d\n", num1*num2);
+  printf("Division is %d\n", num1/num2);
+  printf("Remainder on division is %d\n", num1%num2);
+  return 0;
+}
