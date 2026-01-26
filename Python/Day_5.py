1ede45619ad97d73a2bfc8eb7782b4a96974e807
Boomathi <boomathi2047@gmail.com>
2026-01-26 23:35:39 +0530

    Create Day_5.py

---
+++ Python/Day_5.py
@@ -0,0 +1,10 @@
+#Relational Operators/ Comparison Operators
+
+num1 = int(input("Enter num1: "))
+num2 = int(input("Enter num2: "))
+print("Equal?", num1==num2)
+print("Not Equal?", num1!=num2)
+print("Less than?", num1<num2)
+print("Greater than?", num1>num2)
+print("Less than or Equal?", num1<=num2)
+print("Greater than or Equal?", num1>=num2)
