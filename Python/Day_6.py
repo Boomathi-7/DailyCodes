4528778d888a6bdccecb3083cb94913bee38dd59
Boomathi <boomathi2047@gmail.com>
2026-01-27 22:42:56 +0530

    Create Day_6.py

---
+++ Python/Day_6.py
@@ -0,0 +1,7 @@
+#Logical Operators
+
+num1 = int(input("Enter num1: "))
+num2 = int(input("Enter num2: "))
+print("AND Operator:", num1&num2)
+print("OR Operator:", num1|num2)
+print("NOT Operator:", ~num1)
