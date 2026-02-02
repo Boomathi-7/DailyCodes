20b5f8c77a843d8f494527cc97ab115b1fd6088b
Boomathi <boomathi2047@gmail.com>
2026-02-02 21:13:57 +0530

    Create Day_9.c

---
+++ C/Day_9.c
@@ -0,0 +1,10 @@
+// Get a number from user and divide by 6 and print the quotient
+
+#include <stdio.h>
+int main(){
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    printf("Answer: %d", num/6);
+    return 0;
+}
