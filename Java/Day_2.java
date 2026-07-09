c64163825f757996e46c7d7284e26194ef111774
Boomathi <boomathi2047@gmail.com>
2026-07-09 10:54:05 +0530

    Add Day_2.java with main method and print statements

---
+++ Java/Day_2.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String args[]){
+        int num = 7;
+        String str = "Java";
+        System.out.println(num);
+        System.out.println(str);
+    }
+}
