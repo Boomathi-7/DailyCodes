b247e1c0f542f697088dfd4748165db0902d0e26
Boomathi <boomathi2047@gmail.com>
2026-07-09 10:57:38 +0530

    Add Java program to read and print user input

---
+++ Java/Day_3.java
@@ -0,0 +1,10 @@
+// Input from user and printing
+
+import java.util.Scanner;
+class Main {
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        System.out.println(num);
+    }
+}
