cf4db17b2901a34c8c50df76b7d495f482d4c936
Boomathi <boomathi2047@gmail.com>
2026-07-16 21:51:17 +0530

    Add Java program demonstrating logical operators

---
+++ Java/Day_6.java
@@ -0,0 +1,13 @@
+// Logical Operators
+
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int num1 = scan.nextInt();
+        int num2 = scan.nextInt();
+        System.out.println("AND Operator: " + (num1&num2));
+        System.out.println("OR Operator: " + (num1|num2));
+        System.out.println("NOT Operator: " + (~num1));
+    }
+}
