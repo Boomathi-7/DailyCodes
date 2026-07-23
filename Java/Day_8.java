48ff318acee13acc937dc068e132d3314f07ad5d
Boomathi <boomathi2047@gmail.com>
2026-07-23 14:39:46 +0530

    Add program to multiply user input by 3

---
+++ Java/Day_8.java
@@ -0,0 +1,11 @@
+// Get a number from user and multiply 3 to that number and print the result
+
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        n *= 3;
+        System.out.println(n);
+    }
+}
